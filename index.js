8408b22149d3bb917183a980c496c63fd920d89f
isenkovska <isenkovska@gmail.com>
2021-04-20 15:25:25 -0400

    index package.json

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+
+const app = express();
+const PORT = 3000;
+
+let cards = [
+        {id: '1', name: 'First Card', status: 'todo', priority: 2},
+        {id: '2', name: 'Second Card', status: 'progress', priority: 5},
+        {id: '3', name: 'Next Card', status: 'review', priority: 10},
+    ];
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.get('/card', (req, res) => {
+    res.send(cards);
+})
+
+app.delete('/card/:cardID', (req, res) => {
+    const cardId = req.params.cardID;
+    cards = cards.filter(el => el.id !== cardId);
+    res.send(cards);
+    console.log(cards);
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+})
