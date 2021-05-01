5203cf8d8e0989c815d06ceb03ac550a97fb2ded
isenkovska <isenkovska@gmail.com>
2021-05-01 17:42:32 -0400

    Create card PATCH

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express();
 const PORT = 3000;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
 let cards = [
         {id: '1', name: 'First Card', status: 'todo', priority: 2},
@@ -21,6 +27,13 @@ app.delete('/card/:cardId', (req, res) => {
     const cardId = req.params.cardId;
     cards = cards.filter(el => el.id !== cardId);
     res.send(cards);
+})
+
+app.post('/card', (req, res) => {
+    console.log(req);
+    const card = req.body;
+    cards.push({id: Math.random().toString(), ...card});
+    res.send('Card created');
 })
 
 app.listen(PORT, () => {
